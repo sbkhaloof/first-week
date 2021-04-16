72529b435ff74904f7c3e5b3ec737b10f97e524f
sbkhaloof <>
2021-04-16 09:11:37 +0000

    adding

---
+++ test.js
@@ -0,0 +1,70 @@
+/*
+1- Write a JavaScript program to know if the input is + or - , " 0 considered as + "?
+*/
+//  console.log('welcome to my test');
+//  console.log('task 1 solution:');
+//  alert('the symbol test');
+// var symbol =prompt('please enter your symbol as (0 for +) or (-)');
+//  if (symbol==0){console.log('the input symbol is +');}
+//  else if (symbol=='-'){console.log('the input symbol is -');}
+//  else console.log ('please try again');
+/*
+2- Write a JavaScript program takes 3 input from the user and print the max on the console?
+*/
+console.log('task 2 solution:');
+alert('enter three numbers to find the max');
+ var num1 = prompt('please enter first numbers');
+ var num2 = prompt('please enter second numbers');
+ var num3 = prompt('please enter thired numbers');
+ console.log ('the numbers are ');
+ console.log(num1);
+ console.log(num2);
+ console.log(num3);
+ if (num1 > num2 && num1 > num3)
+ {console.log ('the max number is '+ num1);}
+ else if(num2 > num1 && num2 > num3)
+ {console.log('the max number is '+ num2);}
+ else console.log ('the max number is '+ num3)
+/*
+3- Write a JavaScript for loop that will iterate from 0 to 20. For each iteration, 
+it will check if the current number is odd or even, and display a message on the console?*/
+// console.log('task 3 solution:');
+// alert('you will enter 20 numbers to check odd or even ');
+//  for (var i=0;i<20;i++)
+  // {
+//  var x=prompt('please enter your number');
+//  if ((x % 2) == 0 || x== 0)
+  // { console.log (x+' even number');}
+  // else console.log(x+' odd number')
+//  }
+/*
+4- Write a JavaScript program to calculate the sum of even numbers from 1-100 ?
+*/
+// console.log('task 4 solution:');
+// alert('we will find the summation of even numbers from 1-100');
+// var sumeven = 0 ;
+// for (var i=1; i<= 100; i++)
+// {
+  // if ((i % 2)==0)
+// {sumeven=sumeven+ i ; }
+// }
+// console.log('the summition of even number from 1-100 is : ' + sumeven);
+
+/*
+5- Write a JavaScript program to take 2 numbers from the user and print 
+ex:-
+num1 = 3
+num2 = 3
+the sum is 6
+the multiplication is 9
+*/
+// console.log('task 5 solution:');
+// alert('enter two numbers to find the summation and multiplication')
+//  var y = parseInt(prompt('please enter first numbers'));
+//  var z = parseInt(prompt('please enter second numbers'));
+//  console.log ('num1 = ' + y) ;
+//  console.log ('num2 = '+ z);
+//  var sum = (y + z);
+//  console.log('the sum is ' + sum)  ;
+//  var multi =y*z ;
+  // console.log ('the multiplication is '+ multi) ;
