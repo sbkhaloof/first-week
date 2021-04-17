75e4ca3f21732aae08974681830ec69c732471e6
sbkhaloof <>
2021-04-17 14:35:59 +0000

    updating

--- test.js
+++ test.js
@@ -1,13 +1,12 @@
 /*
 1- Write a JavaScript program to know if the input is + or - , " 0 considered as + "?
 */
-//  console.log('welcome to my test');
-//  console.log('task 1 solution:');
-//  alert('the symbol test');
-// var symbol =prompt('please enter your symbol as (0 for +) or (-)');
-//  if (symbol==0){console.log('the input symbol is +');}
-//  else if (symbol=='-'){console.log('the input symbol is -');}
-//  else console.log ('please try again');
+  console.log('task 1 solution:');
+  alert('test  possitive or negative number');
+ var num_test =prompt('please enter your number');
+  if (num_test>=0){console.log('the input number is possitive '+ num_test);}
+  else{console.log('the input number is negative '+num_test);}
+  
 /*
 2- Write a JavaScript program takes 3 input from the user and print the max on the console?
 */
@@ -28,27 +27,27 @@ alert('enter three numbers to find the max');
 /*
 3- Write a JavaScript for loop that will iterate from 0 to 20. For each iteration, 
 it will check if the current number is odd or even, and display a message on the console?*/
-// console.log('task 3 solution:');
-// alert('you will enter 20 numbers to check odd or even ');
-//  for (var i=0;i<20;i++)
-  // {
-//  var x=prompt('please enter your number');
-//  if ((x % 2) == 0 || x== 0)
-  // { console.log (x+' even number');}
-  // else console.log(x+' odd number')
-//  }
+ console.log('task 3 solution:');
+ alert('you will enter 20 numbers to check odd or even ');
+  for (var i=0;i<20;i++)
+ {
+  var x=prompt('please enter your number');
+  if ((x % 2) == 0 || x== 0)
+ { console.log (x+' even number');}
+ else console.log(x+' odd number')
+  }
 /*
 4- Write a JavaScript program to calculate the sum of even numbers from 1-100 ?
 */
-// console.log('task 4 solution:');
-// alert('we will find the summation of even numbers from 1-100');
-// var sumeven = 0 ;
-// for (var i=1; i<= 100; i++)
-// {
-  // if ((i % 2)==0)
-// {sumeven=sumeven+ i ; }
-// }
-// console.log('the summition of even number from 1-100 is : ' + sumeven);
+ console.log('task 4 solution:');
+ alert('we will find the summation of even numbers from 1-100');
+ var sumeven = 0 ;
+ for (var i=1; i<= 100; i++)
+ {
+ if ((i % 2)==0)
+ {sumeven=sumeven+ i ; }
+ }
+ console.log('the summition of even number from 1-100 is : ' + sumeven);
 
 /*
 5- Write a JavaScript program to take 2 numbers from the user and print 
@@ -58,13 +57,13 @@ num2 = 3
 the sum is 6
 the multiplication is 9
 */
-// console.log('task 5 solution:');
-// alert('enter two numbers to find the summation and multiplication')
-//  var y = parseInt(prompt('please enter first numbers'));
-//  var z = parseInt(prompt('please enter second numbers'));
-//  console.log ('num1 = ' + y) ;
-//  console.log ('num2 = '+ z);
-//  var sum = (y + z);
-//  console.log('the sum is ' + sum)  ;
-//  var multi =y*z ;
-  // console.log ('the multiplication is '+ multi) ;
+ console.log('task 5 solution:');
+ alert('enter two numbers to find the summation and multiplication')
+  var y = parseInt(prompt('please enter first numbers'));
+  var z = parseInt(prompt('please enter second numbers'));
+  console.log ('num1 = ' + y) ;
+  console.log ('num2 = '+ z);
+  var sum = (y + z);
+  console.log('the sum is ' + sum)  ;
+  var multi =y*z ;
+ console.log ('the multiplication is '+ multi) ;
